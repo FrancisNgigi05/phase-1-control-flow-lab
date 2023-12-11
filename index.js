bb17f7e3028d5557fa23021b67aae87bffc551e7
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-11 12:44:33 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+let distanceInFeet = 199;
+let result;
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet < 400) {
+    result = "This one is on me!";
+  }
+  else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else {
+    result = "No can do."
+  }
+  return (result)
 }
 
-function ternaryCheckCity(){
+let city = "NYC";
+function ternaryCheckCity(city){
   // Write your code here!
+  // returns "Ok sounds good." when the city is NYc returns "No go" if city is not NYC
+  result = city === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return (result)
 }
 
-function switchOnCharmFromTip(){
+let tip = "generous";
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye."
+  }
+  return (result);
 }
